210ce2d3f3cc0cab0d2b6f62b3488affe1b008f1
Dillon <dillon@nixx.co>
2013-12-15 09:28:13 +0300

    Basic user classes with PDO

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+require 'core/init.php';
+$general->logged_in_protect();
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css" >
+	<title>Welcome to ICMS</title>
+</head>
+<body>	
+	<div id="container">
+		<?php include 'includes/menu.php'; ?>
+
+		<h1>ICMS - Intelligent Content Management System</h1>
+		<p>Currently Under Construction...</p>
+	</div>
+</body>
+</html>
