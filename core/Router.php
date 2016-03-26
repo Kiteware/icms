5ebd1c35c34257184b445c1a68dcde3056f7fd83
Dillon <dillon@nixx.co>
2016-03-25 20:38:15 -0400

    More casing fixes

--- core/Router.php
+++ core/Router.php
@@ -18,15 +18,15 @@ class Router {
                 $this->table['controller'] = new Route('Nixhatter\ICMS\model\PagesModel', 'admin', 'pagesController', true);
                 return $this->table['controller'];
             } else {
-                $this->table[$controller] = new Route('Nixhatter\ICMS\model\\'.$model . 'Model', $controller, $model . 'Controller', true);
+                $this->table[$controller] = new Route('Nixhatter\ICMS\model\\'.ucfirst($model) . 'Model', $controller, $model . 'Controller', true);
                 return $this->table[$controller];
             }
         } else {
             if ($model == "blog") {
-              $this->table['controller'] = new Route('Nixhatter\ICMS\model\blogModel', 'blog', 'blogController', false);
+              $this->table['controller'] = new Route('Nixhatter\ICMS\model\BlogModel', 'blog', 'blogController', false);
               return $this->table['controller'];
             } else {
-                $this->table[$controller] = new Route('Nixhatter\ICMS\model\\'.$model . 'Model', $controller, $controller . 'Controller', false);
+                $this->table[$controller] = new Route('Nixhatter\ICMS\model\\'.ucfirst($model) . 'Model', $controller, $controller . 'Controller', false);
                 return $this->table[$controller];
             }
         }
