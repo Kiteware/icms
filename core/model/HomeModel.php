a5b1d05534ce06663af3f51e6f8b9dae5393c63f
Dillon <dillon@nixx.co>
2016-03-25 20:04:10 -0400

    Git casing fix

---
+++ core/model/HomeModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * ICMS - Intelligent Content Management System
+ *
+ * @package ICMS
+ * @author Dillon Aykac
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Model
+|--------------------------------------------------------------------------
+|
+| Basic Model Class - Called on /index.php
+|
+*/
+namespace Nixhatter\ICMS\model;
+
+class HomeModel extends Model {
+
+}
