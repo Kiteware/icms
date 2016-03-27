aea582d65131632e862bc2819da7ed70a186d7b1
Dillon <dillon@nixx.co>
2016-03-27 16:48:08 -0400

    template fix

--- templates/default/menu.php
+++ templates/default/menu.php
@@ -5,7 +5,7 @@ namespace Nixhatter\ICMS;
     <ul class="main-nav">
         <div class="boxed">
         <?php
-        $pages    = new model\pagesModel($this->container);
+        $pages    = new model\PagesModel($this->container);
         $navigation = $pages->list_nav();
         foreach ($navigation as $showNav) {
                 echo "<li><a href=\"".$showNav['nav_link']."\">".$showNav['nav_name']."</a></li>";
