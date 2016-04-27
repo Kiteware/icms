5928cbe40db9dde1248dd53d9711306274ef2bc1
Dillon <dillon@nixx.co>
2016-04-27 16:41:17 -0400

    Small blog view changes
    Changed how dates were shown and some html.

--- pages/blog.php
+++ pages/blog.php
@@ -5,31 +5,29 @@ if (count(get_included_files()) ==1) {
 }
 $Parsedown = new Parsedown();
 $posts = $this->model->posts;
-$content = $Parsedown->text($posts['post_content']);
 ?>
 <section class="content">
     <article>
     <?php
-        if (!is_array($posts)) {
-            ?>
-            <h1 class="title"> <a href="/blog/view/<?php echo $post['post_id']?>"><?php echo $posts['post_name'] ?></a></h1>
-            <hr>
+        if (count($posts) === 1) {
+            // View one blog post
+            $content = $Parsedown->text($posts[0]['post_content']); ?>
+            <h1 class="title"> <?php echo $posts[0]['post_name'] ?></h1>
+            <p class="text-muted"><?php echo date('F j, Y', strtotime($post['post_date'])) ?></p>
             <p><?php echo $content ?></p>
-            <p class="date"><?php echo date_format(date_create($posts['post_date']), "F j, Y") ?></p>
-            <hr>
             <?php
         } elseif (empty($posts)) {
-            ?>
+            // No Blog Posts ?>
             <p> There are currently no blog posts.</p>
             <?php
-        }
-        else {
+        } else {
+            // Multiple Blog Posts
             foreach ($posts as $post) {
                 $content = $Parsedown->text($post['post_content']); ?>
-                <hr />
-                <h1><?php echo $post['post_name']?></h1>
+                <h1><a href="/blog/view/<?php echo $post['post_id']?>"><?php echo $post['post_name']?></a></h1>
                 <p class="text-muted"><?php echo date('F j, Y', strtotime($post['post_date'])) ?></p>
                 <p><?php echo $this->model->truncate($content,"<a href=\"/blog/view/".$post['post_id']."\">Read more</a>") ?></p>
+                <hr />
     <?php   }
         } ?>
     </article>
