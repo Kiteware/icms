18b1428501609b73552c01d27b2f525667d493c5
Dillon <dillon@nixx.co>
2016-05-10 15:50:22 -0400

    Changed PayPal button
    https://developer.paypal.com/docs/classic/paypal-payments-standard/ht_create-pps-buttons/

--- pages/payment.php
+++ pages/payment.php
@@ -3,25 +3,20 @@
  * ICMS
  * Payment
  * A generic payment page, this is where you enter the paypal details.
+ * https://developer.paypal.com/docs/classic/adaptive-payments/ht_ap-embeddedPayment-curl-etc/
  * User: Dillon
  * Date: 5/8/2016
  * Time: 11:45 PM
  */
 
 ?>
-<script src="https://www.paypalobjects.com/js/external/apdg.js" type="text/javascript"></script>
 <div class="container section-md">
     <div class="row">
-        <form action="https://www.sandbox.paypal.com/webapps/adaptivepayment/flow/pay" target="PPDGFrame" class="standard">
-
-            <label for="buy">Buy Now:</label>
-            <input type="image" id="submitBtn" value="Pay with PayPal" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif">
-            <input id="type" type="hidden" name="expType" value="mini">
-            <input id="paykey" type="hidden" name="paykey" value="<?php echo $this->controller->paypalPayKey ?>">
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="ENTERIDHERE">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
         </form>
-        <script type="text/javascript" charset="utf-8">
-            var dgFlowMini = new PAYPAL.apps.DGFlowMini({trigger: 'submitBtn'});
-        </script>
-        </body>
     </div>
 </div>
